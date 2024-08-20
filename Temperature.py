04e931affe47bb1c8b4fe3c2a2e3cfe890236280
Anushree <saroja.deshapande99@gmail.com>
2024-08-20 13:26:53 +0530

    All Datastructure program

---
+++ Temperature.py
@@ -0,0 +1,6 @@
+# Python Program to convert Fahrenheit to celsius
+
+fahrenheit = 99
+celseius = (fahrenheit-32)/1.8
+
+print('%.2f fahernheit is equivalent to: %.2f celseius' %(fahrenheit, celseius))
