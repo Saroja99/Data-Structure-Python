dfc3b0a9545aa771592634c771696468695f61e4
Anushree <saroja.deshapande99@gmail.com>
2024-08-20 14:14:07 +0530

    2nd program

---
+++ Radius_of_Circle.py
@@ -0,0 +1,18 @@
+# this program uses import math 
+
+import math as M
+
+Radius = float(input("please enter the radius of the given circle"))
+
+area_of_circle = M.pi*Radius*Radius
+print("Area of given circle is:",area_of_circle)
+
+
+
+
+# calculate the area of given radius using pi
+
+pi = 3.142
+Radius = float(input("please enter the radius of the given circle"))
+area_of_circle = pi*Radius*Radius
+print("Area of given circle is:",area_of_circle)
